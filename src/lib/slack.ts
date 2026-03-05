aa3f76f0ae1565984514cd10ad775c12dee7d867
rod0622 <rod@trytenkara.com>
2026-03-06 07:18:08 +0800

    fix: add slack stub

---
+++ src/lib/slack.ts
@@ -0,0 +1,4 @@
+export async function notifyNewEmail(...args: any[]) {}
+export async function notifyAssignment(...args: any[]) {}
+export async function notifyTaskCreated(...args: any[]) {}
+export async function notifyNote(...args: any[]) {}
